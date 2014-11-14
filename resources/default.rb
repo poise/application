14b7d14c12fc754965de103a70603f60587f9220
Diego Rodriguez <drodriguez@opengov.com>
2014-11-13 17:28:06 -0800

    added rollback support

--- resources/default.rb
+++ resources/default.rb
@@ -28,7 +28,7 @@ def initialize(*args)
   @sub_resources = []
 end
 
-actions :deploy, :force_deploy
+actions :deploy, :force_deploy, :rollback
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :environment_name, :kind_of => String, :default => (node.chef_environment =~ /_default/ ? "production" : node.chef_environment)
