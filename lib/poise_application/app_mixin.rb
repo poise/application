28684c55bdaeadbe802c1ee304ce99c117cb02b1
Noah Kantrowitz <noah@coderanger.net>
2015-08-11 18:06:10 -0700

    Move the AppMixin class level stuffs to actual class level.
    Also docstrings!

--- lib/poise_application/app_mixin.rb
+++ lib/poise_application/app_mixin.rb
@@ -21,12 +21,30 @@ require 'poise/utils'
 
 
 module PoiseApplication
+  # A helper mixin for application resources and providers. These are things
+  # intended to be used as subresources of the `application` resource.
+  #
+  # @since 5.0.0
   module AppMixin
     include Poise::Utils::ResourceProviderMixin
 
+    # A helper mixin for application resources.
     module Resource
       include Poise::Resource
 
+      # Set the parent type and optional flag.
+      poise_subresource(:application, true)
+
+      # @!attribute path
+      #   Base path for the application.
+      #   @return [String]
+      attribute(:path, kind_of: String, name_attribute: true)
+
+      # A delegator for accessing the application state. If no application
+      # parent is found, the state will be tracked internally within the
+      # resource.
+      #
+      # @return [Hash<Symbol, Object>]
       def app_state
         if parent
           parent.app_state
@@ -36,21 +54,12 @@ module PoiseApplication
         end
       end
 
+      # Environment variables stored in the application state.
+      #
+      # @return [Hash<String, String>]
       def app_state_environment
         app_state[:environment] ||= Mash.new
       end
-
-      module ClassMethods
-        # @api private
-        def included(klass)
-          super
-          klass.extend(ClassMethods)
-          klass.poise_subresource(:application, true)
-          klass.attribute(:path, kind_of: String, name_attribute: true)
-        end
-      end
-
-      extend ClassMethods
     end
 
     module Provider
